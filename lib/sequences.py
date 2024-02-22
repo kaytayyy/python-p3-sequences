cf92d7887939b2d80c7a69dd2dc8cee1769c4a32
Inda Smith <inda.k.smith@gmail.com>
2024-02-22 04:50:31 -0500

    this one was tricky, it was quite tedious in the way that the code needed to be written

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_list = [0, 1]
+    if length == 0:
+        print("[]")
+    elif length == 1:
+        print("[0]")
+    elif length == 2:
+        print("[0, 1]")
+    else:
+        while len(fibonacci_list) < length:
+            next_number = fibonacci_list[-1] + fibonacci_list[-2]
+            fibonacci_list.append(next_number)
+        print("[{}]".format(", ".join(map(str, fibonacci_list))))
+
+# print_fibonacci(0)
+# print_fibonacci(1)
+# print_fibonacci(2)
+# print_fibonacci(3)
+# print_fibonacci(8)
